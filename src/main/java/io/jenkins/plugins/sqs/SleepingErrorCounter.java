568c0e3df26ef7a0cb6cdbe579ee8400577b0a65
clement <clement.denis@gmail.com>
2024-04-26 10:49:07 +0200

    Deregister trigger if: - queue does not exist anymore - too many consecutive error occur

---
+++ src/main/java/io/jenkins/plugins/sqs/SleepingErrorCounter.java
@@ -0,0 +1,46 @@
+package io.jenkins.plugins.sqs;
+
+import com.google.common.collect.ConcurrentHashMultiset;
+import com.google.common.collect.Multiset;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This counter will sleep an increased amount of time every error, until it reaches max errors.
+ *
+ * Max sleep time is maxErrorCount(maxErrorCount + 1) / 2 * sleepTimeMultiplierSeconds.
+ * For defaults: 5 * 6 / 2 * 10 = 150 seconds
+ * @see https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+ */
+public class SleepingErrorCounter {
+
+    private final Multiset<String> counter = ConcurrentHashMultiset.create();
+
+    private final int maxErrorCount;
+
+    private final int sleepTimeMultiplierSeconds;
+
+    public SleepingErrorCounter() {
+        this(5, 10);
+    }
+
+    public SleepingErrorCounter(int maxErrorCount, int sleepTimeMultiplierSeconds) {
+        this.maxErrorCount = maxErrorCount;
+        this.sleepTimeMultiplierSeconds = sleepTimeMultiplierSeconds;
+    }
+
+    void reset(String key) {
+        counter.setCount(key, 0);
+    }
+
+    @SneakyThrows
+    boolean tooManyErrors(String key) {
+        boolean tooManyErrors = counter.add(key, 1) >= maxErrorCount;
+        if (!tooManyErrors) {
+            TimeUnit.MINUTES.sleep((long) sleepTimeMultiplierSeconds * counter.count(key));
+        }
+        return tooManyErrors;
+    }
+
+}
